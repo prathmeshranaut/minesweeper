033c9f4ea7175dae82332e572805b4bcfa5de87f
Aayush Ranaut <aayush.ranaut@gmail.com>
2019-11-17 18:44:38 -0500

    Initial Commit

---
+++ app/games/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
+
+
+class Game(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    game_field = models.TextField()
+    open_tile = models.TextField(blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
